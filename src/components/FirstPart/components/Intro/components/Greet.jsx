a3f90cf85cc3f53bf7a7ed92e5f164d4ba865acd
luqman9000 <luqmanhal9000@gmail.com>
2024-03-21 14:18:52 +0800

    Fix able to click the whole line

--- src/components/FirstPart/components/Intro/components/Greet.jsx
+++ src/components/FirstPart/components/Intro/components/Greet.jsx
@@ -24,15 +24,19 @@ const Greet = () => {
             <p style={{ fontFamily: "Thasadith", fontSize:"25px" }}>
                 My name is <b style={{ fontFamily: "Thasadith", fontSize:"30px" }}>Muhammad Luqman</b>. I have a passion for all things <b style={{ fontFamily: "Thasadith", fontSize:"30px" }}>tech</b>, and I work as a <b style={{ fontFamily: "Thasadith", fontSize:"30px" }}>developer</b>. Nonetheless, I am always open to <b style={{ fontFamily: "Thasadith" }}>learning</b> and <b style={{ fontFamily: "Thasadith" }}>exploring</b> new concepts.
             </p>
-            <div className="medium" style={{ display: 'flex', flexDirection: matches ? 'column' : 'row', justifyContent: matches ? 'initial' : 'center', gap: matches ? '0px' : '10px' }}>
-                {matches && <p>My Blog</p>}
-                <a href="https://github.com/luqman9000" aria-label="Medium Blog">
-                    <BsMedium size={40}/>
-                </a>
-                {matches && <p>My Github profile</p>}
-                <a href="https://github.com/luqman9000" aria-label="Github">
-                    <FaGithub size={40}/>
-                </a>
+            <div className="medium" style={{ display: 'flex', flexDirection: matches ? 'column' : 'row', justifyContent: matches ? 'normal' : 'center', gap: matches ? '0px' : '10px' }}>
+                {matches && <div><p>My Blog</p></div>}
+                <div>
+                    <a href="https://medium.com/@luqmanhal9000" aria-label="Medium Blog" target="_blank" rel="noreferrer">
+                        <BsMedium size={40}/>
+                    </a>
+                </div>
+                {matches && <div><p>My Github</p></div>}
+                <div>
+                    <a href="https://github.com/luqman9000" aria-label="Github" target="_blank" rel="noreferrer">
+                        <FaGithub size={40}/>
+                    </a>
+                </div>
             </div>
         </div>
     )
